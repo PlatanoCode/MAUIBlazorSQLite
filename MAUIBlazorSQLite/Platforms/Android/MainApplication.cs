8760e545f61c82372b6550a2af6e032d09c56ffc
Steven Checo <steven.checo.19@gmail.com>
2022-06-25 05:37:35 -0400

    remove sqlite initialization from Android platform

--- MAUIBlazorSQLite/Platforms/Android/MainApplication.cs
+++ MAUIBlazorSQLite/Platforms/Android/MainApplication.cs
@@ -14,7 +14,7 @@ public class MainApplication : MauiApplication
 
 	protected override MauiApp CreateMauiApp() 
 	{
-        raw.SetProvider(new SQLite3Provider_sqlite3());
+        //raw.SetProvider(new SQLite3Provider_sqlite3());
         return MauiProgram.CreateMauiApp();
     }
 }
